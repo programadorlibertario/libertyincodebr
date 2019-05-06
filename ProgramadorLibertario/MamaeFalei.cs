e551a206801a45ec9e5584c68350a0ae6fd4c2c0
Alexandre Lima <alexandreslima@outlook.com>
2019-05-06 17:58:08 -0300

    add

---
+++ ProgramadorLibertario/MamaeFalei.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramadorLibertario
+{
+    class MamaeFalei
+    {
+        public bool VamosQuestionarTudo(PersonType person)
+        {
+            if (person == PersonType.Ancap)
+                return false;
+
+            return true;
+        }
+    }
+
+    public enum PersonType
+    {
+        EsquerdistaSafado,
+        Ancap
+    }
+}
